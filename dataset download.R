6d9ab6a9ddc902067daa775bbc4325a06f8190b6
ayushman129 <ayushkumar129@gmail.com>
2023-03-08 16:11:13 +0530

    dataset downloader added

---
+++ dataset download.R
@@ -0,0 +1,8 @@
+##This program downloads the dataset if not available in the directory.
+
+if(!file.exists("household_power_consumption.txt")) {
+  temp <- tempfile()
+  download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+  file <- unzip(temp)
+  unlink(temp)
+}
