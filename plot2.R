99bb018ac41d91cdf5f2c2657a5f544d2ae68903
ayushman129 <ayushkumar129@gmail.com>
2023-03-08 16:16:41 +0530

    plot 2 added

---
+++ plot2.R
@@ -0,0 +1,15 @@
+##Set language to English (days in the x-axis)
+Sys.setlocale("LC_TIME", "English")
+
+##Format the time vector
+date_converted <- as.Date(data[, 1], format = "%d/%m/%Y")
+time_converted <- strptime(data[, 2], format = "%H:%M:%S")
+time_axis <- as.POSIXct(paste(date_converted, data[, 2]))
+
+##Plot global active power against time vector
+plot(time_axis, data[, 3], xlab = "", ylab = "Global Active Power (kilowatts)", type = "l")
+
+##saving the plot as png file of required dimensions
+dev.copy(png, file="plot2.png", width=480, height=480)
+dev.off()
+cat("Plot2.png has been saved in", getwd())
